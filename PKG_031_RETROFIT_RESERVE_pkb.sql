1c70424d71660772da47b3844835fb64b93f9a5b
Steve Schafer <sschafer@innoventsolutions.com>
2016-09-14 16:00:20 -0600

    revert PROD_CODE_XREF_RCPO_DR joins to use PS columns

--- PKG_031_RETROFIT_RESERVE_pkb.sql
+++ PKG_031_RETROFIT_RESERVE_pkb.sql
@@ -286,8 +286,8 @@ CREATE OR REPLACE PACKAGE BODY DBO.pkg_031_retrofit_reserve IS
                       and MLR.RETRO_ID = RD.RETROFIT_ID
                       -- NEW_RETROFIT_ID --
                       AND (CASE WHEN CACCT.CUST_CREDIT_CATG_CODE='Z1' THEN 'Y' ELSE 'N' END)=RES_PCT.CUST_CREDIT_CATG_CODE
-                      AND GLA.R12_ENTITY /* -SS- COMPANY */=PRODGRP.GL_LEDGER /* -SS- FIXME */
-                      AND PCS.PROD_CODE = PRODGRP.ORA_PRODUCT -- -SS- MANF_PROD_CODE, FIXME join won't work
+                      AND GLA.PS_COMPANY = PRODGRP.GL_LEDGER
+                      AND PCS.PROD_CODE = PRODGRP.MANF_PROD_CODE
                       AND PRODGRP.PRODUCT_CATEGORY IS NOT NULL
                       and TD3.FULL_DATE >= TO_DATE('1/1/2001','MM/DD/YYYY')
                       --and TD3.FULL_DATE <= TO_DATE('12/31/2008','MM/DD/YYYY')
@@ -487,8 +487,8 @@ CREATE OR REPLACE PACKAGE BODY DBO.pkg_031_retrofit_reserve IS
                       AND CCN_DATA.EXPENSE_TYPE_CATG= UPPER(RES_PCT.EXPENSE_TYPE_CATG)
                       AND SOS.COMPANY_OWNED_IND=RES_PCT.COMPANY_OWNED_IND
                       AND (CASE WHEN CACCT.CUST_CREDIT_CATG_CODE='Z1' THEN 'Y' ELSE 'N' END)=RES_PCT.CUST_CREDIT_CATG_CODE
-                      AND GLA.R12_ENTITY /* -SS- COMPANY */=PRODGRP.GL_LEDGER /* -SS- FIXME */
-                      AND PCS.PROD_CODE = PRODGRP.ORA_PRODUCT -- -SS- MANF_PROD_CODE, FIXME join won't work
+                      AND GLA.PS_COMPANY = PRODGRP.GL_LEDGER
+                      AND PCS.PROD_CODE = PRODGRP.MANF_PROD_CODE
                       AND PRODGRP.PRODUCT_CATEGORY IS NOT NULL
                       -- NEW_RETROFIT_ID -
                       and CCN_DATA.RETRO_ID = RD.RETROFIT_ID

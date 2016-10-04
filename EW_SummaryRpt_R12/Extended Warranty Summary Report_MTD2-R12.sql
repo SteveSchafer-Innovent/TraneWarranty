03ad8ab23e53da70f1c6b07737cc81e6d7d7792d
Steve Schafer <sschafer@innoventsolutions.com>
2016-10-04 11:25:13 -0600

    Some issue numbers corrected. Some R12_2_R12 changed to ok. Some ???? removed because issue was resolved. Fixes in PKG_030_EXT_WARRANTY to make it compile.

--- EW_SummaryRpt_R12/Extended Warranty Summary Report_MTD2-R12.sql
+++ EW_SummaryRpt_R12/Extended Warranty Summary Report_MTD2-R12.sql
@@ -26,8 +26,7 @@ FROM
   RIGHT OUTER JOIN
     /*TAY:           OTR_TRANE_ACCOUNTS_PS psa,*/
     R12_TRANE_ACCOUNTS_PS PSA
-  ON gl_code_combinations.Segment4 = PSA.R12_Account
-    /* R12_2_R12 */
+  ON gl_code_combinations.Segment4 = PSA.R12_Account -- R12_2_R12
     /*,
     (SELECT a.BUSINESS_UNIT PS_BU ,
     A.ORACLE_XREF_VALUE Oracle_BU
@@ -139,8 +138,7 @@ RIGHT OUTER JOIN
   RIGHT OUTER JOIN
     /*TAY:           OTR_TRANE_ACCOUNTS_PS psa,*/
     R12_TRANE_ACCOUNTS_PS PSA
-  ON gl_code_combinations.Segment4 = PSA.R12_Account
-    /* R12_2_R12 */
+  ON gl_code_combinations.Segment4 = PSA.R12_Account -- R12_2_R12
     /*TAY:        (SELECT a.BUSINESS_UNIT PS_BU ,
     A.ORACLE_XREF_VALUE Oracle_BU
     FROM dbo.ps_trane_R12_xref   a

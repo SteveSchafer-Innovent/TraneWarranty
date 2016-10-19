ee0e2c18f0b1d01c3470dd73295996d1af634327
Scott Rosenbaum <scottr@innoventsolutions.com>
2016-10-19 13:34:41 -0500

    Add more debug by counts

--- Maps/count_cache_table.sql
+++ Maps/count_cache_table.sql
@@ -3,3 +3,12 @@ select '2_25_RC79', count(*) from IOB_25_RC79 union all
 select '3_25_WEXD68', count(*) from IOB_25_WEXD68 union all
 select '3_25_WSMT68', count(*) from IOB_25_WSMT68 union all
 select '3_25_WRRC68', count(*) from IOB_25_WRRC68;
+
+select '1_IOB_WC_MAPSL', count(*) from IOB_WC_MAPSL union all
+select '2_IOB_WC_CEXP', count(*) from IOB_WC_CEXP union all
+select '3_IOB_WC_WEXP', count(*) from IOB_WC_WEXP union all
+select '4_IOB_WC_RC66', count(*) from IOB_WC_RC66 union all
+select '5_IOB_WC_CSMT', count(*) from IOB_WC_CSMT union all
+select '6_IOB_WC_WSMT', count(*) from IOB_WC_WSMT union all
+select '7_IOB_WC_CRRC', count(*) from IOB_WC_CRRC union all
+select '8_IOB_WC_WRRC', count(*) from IOB_WC_WRRC;

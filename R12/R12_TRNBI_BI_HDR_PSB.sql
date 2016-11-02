4cacf9341568a3dcb7bed4a16a7ba6f2c83fcf7d
Scott Rosenbaum <scottr@innoventsolutions.com>
2016-11-02 10:52:29 -0500

    This is the code that was sent over to Amar for Informatica.

--- R12/R12_TRNBI_BI_HDR_PSB.sql
+++ R12/R12_TRNBI_BI_HDR_PSB.sql
@@ -1,12 +1,28 @@
 -- R12_TRNBI_BI_HDR_PSB;
-select
-'NA' as BUSINESS_UNIT,
-RCT.TRX_NUMBER as INVOICE,
-RCT.ATTRIBUTE9 as TRNBI_PROJECT_TYPE,
-'' as SRC_ED_CREATE_DATE,
-'' as SRC_ED_CREATE_ID,
-'' as ED_CREATE_DATE,
-'' as ED_CREATE_ID,
-RCT.CUSTOMER_TRX_ID as CUSTOMER_TRX_ID
-from
-RA_CUSTOMER_TRX_ALL RCT;
+SELECT DISTINCT
+		'NA' AS BUSINESS_UNIT,
+		RCT.TRX_NUMBER AS INVOICE,
+		RCT.ATTRIBUTE9 AS TRNBI_PROJECT_TYPE,
+		'' AS SRC_ED_CREATE_DATE,
+		'' AS SRC_ED_CREATE_ID,
+		'' AS ED_CREATE_DATE,
+		'' AS ED_CREATE_ID,
+		RCT.CUSTOMER_TRX_ID AS CUSTOMER_TRX_ID
+	FROM
+		RA_CUSTOMER_TRX_ALL RCT
+	INNER JOIN RA_BATCH_SOURCES_ALL RBS ON RBS.BATCH_SOURCE_ID = RCT.BATCH_SOURCE_ID AND RBS.ORG_ID = RCT.ORG_ID
+	WHERE
+		0 = 0
+		AND RCT.ATTRIBUTE9 = '7'
+		AND RBS.BATCH_SOURCE_ID IN('90003', '90006', '87004', '87003', '90005')
+	ORDER BY 1 ;
+/*
+select distinct rct.batch_source_id
+from ra_customer_trx_all rct, ra_batch_sources_all bs
+where rct.batch_source_id = bs.batch_source_id
+and rct.org_id = bs.org_id
+and rct.org_id in ('456', '457')
+and rct.attribute9 = '7'
+order by 1
+*/
+;

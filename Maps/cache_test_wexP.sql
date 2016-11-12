91a390123d7d1fcc02668d9f75779dcf4dfccc97
Scott Rosenbaum <scottr@innoventsolutions.com>
2016-11-12 12:30:01 -0600

    Lots of debug on space issue.

---
+++ Maps/cache_test_wexP.sql
@@ -0,0 +1,59 @@
+-- MAP_EXPENSEWARRANTYDATA68_VW
+SELECT
+		TRX_YEAR,
+		CLAIM_TYPE,
+		SUM(ROUND(EXPENSE_AMOUNT,2))
+	FROM
+		IOB_WC_WEXP
+	GROUP BY
+		TRX_YEAR,
+		CLAIM_TYPE
+	ORDER BY 1,2,3;
+	
+	select *
+from iob_25_wrrc68 
+where jrnl_year_month > 201500
+order by jrnl_year_month desc, 1;
+
+select country_indicator, jrnl_year_month, months_from_base, sum(round(revenue_amount,2)) rev
+from IOB_25_WSMT68
+group by country_indicator, jrnl_year_month, months_from_base
+order by 1,2,3,4;
+
+SELECT
+		COUNTRY_INDICATOR,
+		SHIP_ROW,
+		TRX_COLUMN,
+		ROW_PLUS_COLUMN,
+		REVENUE_AMOUNT,
+		JRNL_YEAR_MONTH
+	FROM
+		IOB_25_WRRC68 
+	where rowNum < 10;
+	
+-- MAP_EXPENSEWARRANTYDATA68_VW
+SELECT
+		claim_number,
+		count(*)
+	FROM
+		IOB_WC_WEXP
+	WHERE TRX_YEAR > 2014
+	and TRX_MONTH = 9
+	and CLAIM_TYPE = 'MATERIAL'
+	group by claim_number
+	ORDER BY 1;
+	
+	
+select * from iob_WC_WEXP where claim_type = 'SPD' and rownum < 100 and trx_year = 2016;
+select distinct trx_year from iob_WC_WEXP where claim_type = 'SPD';
+select distinct trx_year from iob_WC_CEXP where claim_type = 'SPD';
+
+select * from iob_WC_CEXP where claim_number = '9352074';
+
+/*
+9144282
+9144285
+9144286
+9144299
+9147049
+*/
--- Maps/count_cache_table.sql
+++ Maps/count_cache_table.sql
@@ -1,10 +1,11 @@
+select '1_25_MSD68', count(*) from IOB_25_MSD68;
 select '1_25_MSD68', count(*) from IOB_25_MSD68 union all
 select '2_25_RC79', count(*) from IOB_25_RC79 union all
 select '3_25_WEXD68', count(*) from IOB_25_WEXD68 union all
 select '3_25_WSMT68', count(*) from IOB_25_WSMT68 union all
 select '3_25_WRRC68', count(*) from IOB_25_WRRC68 union all
 
-select '1_IOB_WC_MAPSL', count(*) from IOB_WC_MAPSL union all
+-- select '1_IOB_WC_MAPSL', count(*) from IOB_WC_MAPSL union all
 select '2_IOB_WC_CEXP', count(*) from IOB_WC_CEXP union all
 select '3_IOB_WC_WEXP', count(*) from IOB_WC_WEXP union all
 select '4_IOB_WC_RC66', count(*) from IOB_WC_RC66 union all
@@ -13,13 +14,14 @@ select '6_IOB_WC_WSMT', count(*) from IOB_WC_WSMT union all
 select '7_IOB_WC_CRRC', count(*) from IOB_WC_CRRC union all
 select '8_IOB_WC_WRRC', count(*) from IOB_WC_WRRC;
 
+desc IOB_25_WRRC68;
 
+/*
 truncate table IOB_25_MSD68 drop storage;
 truncate table IOB_25_RC79 drop storage;
 truncate table IOB_25_WEXD68 drop storage;
 truncate table IOB_25_WSMT68 drop storage;
 truncate table IOB_25_WRRC68 drop storage;
-truncate table IOB_WC_MAPSL drop storage;
 truncate table IOB_WC_CEXP drop storage;
 truncate table IOB_WC_WEXP drop storage;
 truncate table IOB_WC_RC66 drop storage;
@@ -27,6 +29,7 @@ truncate table IOB_WC_CSMT drop storage;
 truncate table IOB_WC_WSMT drop storage;
 truncate table IOB_WC_CRRC drop storage;
 truncate table IOB_WC_WRRC drop storage;
+*/
 
 
 select query_source, jrnl_year, round(sum(revenue_amount),6), round(sum(revenue_amount_dec),6)
@@ -36,6 +39,9 @@ group by query_source, jrnl_year order by 2,1;
 
 select jrnl_year, sum(revenue_amount), sum(revenue_amount_dec) from iob_25_MSD68_2 group by jrnl_year order by 1;
 
+select query_source, count(*) from IOB_25_MSD68 group by query_source order by 1;
+select query_source, count(*) from IOB_25_MSD68 group by query_source order by 1;
 
-select query_source, count(*) from IOB_25_MSD68 group by query_source order by 1;
-select query_source, count(*) from IOB_25_MSD68 group by query_source order by 1;
+create table IOB_25_MSD68_back as Select * from IOB_25_MSD68;
+
+select count(*) from dbo.map_warranty_expense_data;
